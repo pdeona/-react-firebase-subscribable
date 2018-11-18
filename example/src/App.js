409797d8101c0d5ce4ddd211313bb0c6dcec5acd
Pedro De Ona <pdeon003@fiu.edu>
2018-11-18 00:11:44 -0500

    hooks example rewrite

--- example/src/App.js
+++ example/src/App.js
@@ -1,67 +1,23 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
+import {
+  useAuthSubscription,
+} from 'react-firebase-subscribable'
 import firebase from './firebase'
 import Container from './components/Container'
 import User from './models/user'
 import './App.css'
 
-class App extends PureComponent {
-  state = {
-    user: null,
-    userProfile: null,
-    userProfileRef: null,
-    color: '',
-    name: '',
-  }
+function App() {
+  const user = useAuthSubscription(firebase.auth())
 
-  authStateChanged = user => this.setState(state => ({
-    user,
-    userProfile: user ? state.userProfile : null,
-    userProfileRef: user ? User.userProfile(user.uid) : null,
-  }))
-
-  onUpdateProfile = p => {
-    const data = p.data()
-    const newProfileState = state => ({
-      userProfile: data,
-      color: data ? data.favoriteColor : '',
-      name: data ? data.name : '',
-    })
-    this.setState(newProfileState)
-  }
-
-  onChangeColor = ({ target: { value: color } }) => User.userProfile(this.state.user.uid).set({ favoriteColor: color }, { merge: true })
-
-  onChangeName = ({ target: { value: name } }) => User.userProfile(this.state.user.uid).set({ name }, { merge: true })
-
-  render() {
-    const {
-      user,
-      userProfile,
-      userProfileRef,
-      name,
-      color,
-    } = this.state
-
-    return (
-      <div className="App">
-        <Container
-          user={user}
-          userProfile={userProfile}
-          updateProfile={this.updateProfile}
-          onAuthStateChanged={this.authStateChanged}
-          firestoreRef={userProfileRef}
-          onSnapshot={this.onUpdateProfile}
-          firebaseAuth={firebase.auth()}
-          form={{
-            name,
-            color,
-          }}
-          onChangeUserColor={this.onChangeColor}
-          onChangeUserName={this.onChangeName}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Container
+        user={user}
+        userProfileRef={user && User.userProfile(user.uid)}
+      />
+    </div>
+  );
 }
 
 export default App;
--- example/src/components/Container.js
+++ example/src/components/Container.js
@@ -1,55 +1,55 @@
 import React from 'react'
-import compose from 'lodash/fp/compose'
-import {
-  withAuthSubscription,
-  withFirestoreSubscription,
-} from 'react-firebase-subscribable'
+import { useFirestoreSubscription } from 'react-firebase-subscribable'
 import User from '../models/user'
+
+const onChangeAttr = attr => user => ({ target: { value } }) => User
+  .userProfile(user.uid).set({ [attr]: value }, { merge: true })
+const onChangeColor = onChangeAttr('favoriteColor')
+const onChangeName = onChangeAttr('name')
 
 function Container({
   user,
-  userProfile,
-  updateProfile,
-  onChangeUserColor,
-  onChangeUserName,
-  form,
+  userProfileRef,
 }) {
+  const userProfileSnap = useFirestoreSubscription(userProfileRef)
+  const userProfile = userProfileSnap && userProfileSnap.data()
+
+  if (user) {
+    return (
+      <div>
+        <div className="profile-info">
+          {userProfile ?
+            `${userProfile.name}'s favorite color is ${userProfile.favoriteColor || 'unknown at this time'}` : 'We dont have your profile yet!'
+          }
+        </div>
+        <form>
+          <label htmlFor="user-name">Name:</label>
+          <input 
+            name="name"
+            id="user-name"
+            onChange={onChangeName(user)}
+          />
+          <label htmlFor="user-color">Fav. Color:</label>
+          <input 
+            name="color" 
+            id="user-color" 
+            onChange={onChangeColor(user)}
+          />
+        </form>
+        <button onClick={User.signOut}>
+          Sign Out
+        </button>
+      </div>
+    )
+  }
   return (
     <div>
-      <div className="profile-info">
-        {user ?
-          userProfile ?
-            `${userProfile.name}'s favorite color is ${userProfile.favoriteColor || 'unknown at this time'}` : 'We dont have your profile yet!' :
-            'Sign in to view profile'
-        }
-      </div>
-      {
-        user &&
-          <form>
-            <label htmlFor="user-name">Name:</label>
-            <input 
-              name="name"
-              id="user-name"
-              value={form.name}
-              onChange={onChangeUserName}
-            />
-            <label htmlFor="user-color">Fav. Color:</label>
-            <input 
-              name="color" 
-              id="user-color" 
-              value={form.color}
-              onChange={onChangeUserColor}
-            />
-          </form>
-      }
-      <button onClick={user ? User.signOut : User.signInAnonymously}>
-        Sign {user ? 'Out' : 'In'}
+      <span>Sign in to view your profile</span>
+      <button onClick={User.signInAnonymously}>
+        Sign In
       </button>
     </div>
   )
 }
 
-export default compose(
-  withAuthSubscription,
-  withFirestoreSubscription,
-)(Container)
+export default Container
